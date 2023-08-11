cef8d99a1676d08f44e8dbf73a49663a09abaa42
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2023-08-11 14:15:17 -0600

    feat: add icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,9 @@
+import ArrowLine from "./ArrowLine.svelte";
+import Education from "./Education.svelte";
+import RocketLaunch from "./RocketLaunch.svelte";
+
+export {
+    ArrowLine,
+    Education,
+    RocketLaunch
+}
