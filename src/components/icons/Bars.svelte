8e1524735008fd178198affa8b9b8783640d9604
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2023-08-12 08:56:01 -0600

    feat: add icons social networks

---
+++ src/components/icons/Bars.svelte
@@ -0,0 +1,18 @@
+<svg 
+    viewBox="0 0 20 20" 
+    fill="currentColor" 
+    class="icon"
+>
+    <path 
+        fill-rule="evenodd" 
+        d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10zm0 5.25a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75z" 
+        clip-rule="evenodd"
+    />
+</svg>
+
+<style>
+    .icon {
+        width: 1.6rem;
+        height: 1.6rem;
+    }
+</style>
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,9 +1,19 @@
 import ArrowLine from "./ArrowLine.svelte";
+import Bars from "./Bars.svelte";
 import Education from "./Education.svelte";
 import RocketLaunch from "./RocketLaunch.svelte";
+import Github from "./Github.svelte";
+import Linkedin from "./Linkedin.svelte";
+import Mail from "./Mail.svelte";
+import Twitter from "./Twitter.svelte";
 
 export {
-    ArrowLine,
-    Education,
-    RocketLaunch
-}
+  ArrowLine,
+  Bars,
+  Education,
+  RocketLaunch,
+  Github,
+  Linkedin,
+  Mail,
+  Twitter,
+};
