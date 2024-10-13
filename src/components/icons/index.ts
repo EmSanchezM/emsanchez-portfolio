fbbba11ad67e7a3a1cab748170c38a00c9ce4c51
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2024-10-13 11:42:54 -0600

    refactor: :recycle: upgrade dependencies and remove svelte

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,11 +1,11 @@
-import ArrowLine from "./ArrowLine.svelte";
-import Bars from "./Bars.svelte";
-import Education from "./Education.svelte";
-import RocketLaunch from "./RocketLaunch.svelte";
-import Github from "./Github.svelte";
-import Linkedin from "./Linkedin.svelte";
-import Mail from "./Mail.svelte";
-import Twitter from "./Twitter.svelte";
+import ArrowLine from "./ArrowLine.astro";
+import Bars from "./Bars.astro";
+import Education from "./Education.astro";
+import RocketLaunch from "./RocketLaunch.astro";
+import Github from "./Github.astro";
+import Linkedin from "./Linkedin.astro";
+import Mail from "./Mail.astro";
+import Twitter from "./Twitter.astro";
 
 export {
   ArrowLine,
