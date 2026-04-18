e13e015f8f08bff9033e7d50c8ca35f38f5bf0e9
Elvin Sanchez <37355673+EmSanchezM@users.noreply.github.com>
2026-04-18 10:32:02 -0600

    Mejoras en SEO y performance (#26)
    * feat: enhance SEO performance with sitemap integration and meta tags
    
    * fix: remove caret (^) from dependency versions in package.json and pnpm-lock.yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: []
+  site: 'https://emsanchez.com',
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  integrations: [sitemap()],
 });
